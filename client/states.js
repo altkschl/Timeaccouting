869ad02f6cebb47a019a9970e4e5df5b098fd611
Kevin Schlosser <kevin.schlosser@flex.com>
2018-01-11 01:41:04 -0800

    [Studio] Add screen "test"

--- client/states.js
+++ client/states.js
@@ -51,5 +51,14 @@ angular.module('app').config(function ($stateProvider) {
                 }
             }
         }
+    }).state('app.test', {
+        views: {
+            app: {
+                controller: 'app_test',
+                templateProvider: function (app) {
+                    return app.templateProvider('app.test');
+                }
+            }
+        }
     });
 });
