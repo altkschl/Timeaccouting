88f45d1a5a9bc3f74ae8a392aa30533dd1c2a500
Kevin Schlosser <kevin.schlosser@flex.com>
2018-01-11 02:27:53 -0800

    [Studio] Update mock actions for "mytimetable"

--- client/mocks/mytimetable.js
+++ client/mocks/mytimetable.js
@@ -1,3 +1,5 @@
 'use strict';
 window.mocks = window.mocks || {};
 window.mocks.mytimetable = window.mocks.mytimetable || {};
+window.mocks.mytimetable.mytimetable_ChooseWeek = function ($scope, params, app) {
+};
