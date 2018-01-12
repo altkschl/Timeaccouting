1f87d1c83bc35c5914c3f43b8cb5fd5b7ec4dfa8
Kevin Schlosser <kevin.schlosser@flex.com>
2018-01-12 00:37:15 -0800

    [Extension] Delete connection from page model "enterhourspopup", Create screen "enterhourspopup1"

--- client/states.js
+++ client/states.js
@@ -60,5 +60,14 @@ angular.module('app').config(function ($stateProvider) {
                 }
             }
         }
+    }).state('app.enterhourspopup1', {
+        views: {
+            app: {
+                controller: 'app_enterhourspopup1',
+                templateProvider: function (app) {
+                    return app.templateProvider('app.enterhourspopup1');
+                }
+            }
+        }
     });
 });
