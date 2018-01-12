415c9033a09903e149d621968f20d50a643962cd
Kevin Schlosser <kevin.schlosser@flex.com>
2018-01-12 02:25:03 -0800

    [Studio] Modified screen "login"

--- client/controllers/app/login.js
+++ client/controllers/app/login.js
@@ -2,7 +2,7 @@ angular.module('app').controller('app_login', app_login);
 function app_login($scope, app,$ionicLoading) {
     'use strict';
     app.init($scope);
-    app.call('login.loginBasic', $scope);
+    app.call('login.loginBasic', $scope.data);
   
 }
 
