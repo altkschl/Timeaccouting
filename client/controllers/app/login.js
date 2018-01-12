da9b743204dcf5195ea73dc166e556f07df5a2da
Kevin Schlosser <kevin.schlosser@flex.com>
2018-01-12 01:40:26 -0800

    [Studio] Modified screen "login"

--- client/controllers/app/login.js
+++ client/controllers/app/login.js
@@ -1,30 +1,28 @@
-/*
-angular.module('app').controller('app_login', app_login);
-function app_login($scope, app, $q, powwowLoginNew) {
+/*angular.module('app').controller('app_login', app_login);
+function app_login($scope, app,$ionicLoading) {
     'use strict';
     app.init($scope);
-    $scope.login = function () {
-        $scope.app.showLoading('Logging in');
-      app.call('login.loginBasic', credentials);
-    };
-*/
+  
+} */
 
 angular.module('app').controller('app_login', app_login);
 function app_login($scope, app, $q, powwowLoginNew) {
     'use strict';
     app.init($scope);
+ 
     $scope.login = function () {
         $scope.app.showLoading('Logging in');
         app.action('login', 'submit', this);
-       // app.call('login.loginBasic', params);
     };
+ 
     app.origEstablishConnection = app.establishConnection;
     app.establishConnection = function (params) {
         if (app.alreadyConnected) {
-            console.log('Calling powwowLoginNew getcachedcredentials');
+            console.log("Calling powwowLoginNew getcachedcredentials");
             var credentials = powwowLoginNew.getCachedCredentials();
             if (!credentials.username) {
-                console.log('No cached credentials');
+                console.log("No cached credentials");
+                // if no user credentials we cannot perform App login - forvard user to loginScreen
                 powwowLoginNew.clearCachedCredentials();
                 window.location.reload();
                 return;
@@ -33,5 +31,5 @@ function app_login($scope, app, $q, powwowLoginNew) {
         } else {
             app.origEstablishConnection(params);
         }
-    };
+    }
 }
