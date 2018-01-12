83cff14710f8508e2b8960bcfc4896d96da0a2d9
Kevin Schlosser <kevin.schlosser@flex.com>
2018-01-12 01:26:10 -0800

    [Studio] Update mock actions for "enterhourspopup"

--- client/mocks/enterhourspopup.js
+++ client/mocks/enterhourspopup.js
@@ -1,3 +1,6 @@
 'use strict';
 window.mocks = window.mocks || {};
 window.mocks.enterhourspopup = window.mocks.enterhourspopup || {};
+window.mocks.enterhourspopup.clickConfirm = function ($scope, params, app) {
+    alert('Hours allocated !');
+};
