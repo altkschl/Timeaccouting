0af907a712417f3f4c5fcfec8654d3a7c407c460
Kevin Schlosser <kevin.schlosser@flex.com>
2018-01-12 04:02:36 -0800

    [Studio] Modified screen "login"

--- client/controllers/app/login.js
+++ client/controllers/app/login.js
@@ -6,7 +6,7 @@ function app_login($scope, app, $q, powwowLoginNew) {
  
     $scope.login = function () {
         $scope.app.showLoading('Logging in');
-        app.action('login','login', this);
+        app.call('login','loginBasic', this);
     };
  
     app.origEstablishConnection = app.establishConnection;
