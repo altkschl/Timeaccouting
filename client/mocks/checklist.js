7ac15bbf37b64dd6d7835726850b8eb849a91d23
Kevin Schlosser <kevin.schlosser@flex.com>
2018-02-27 03:30:46 -0800

    [Extension] Create screen "checklist", Connect screens to page model "checklist", Update model in connection of page model "checklist"

---
+++ client/mocks/checklist.js
@@ -0,0 +1,3 @@
+'use strict';
+window.mocks = window.mocks || {};
+window.mocks.checklist = window.mocks.checklist || {};
--- client/states.js
+++ client/states.js
@@ -51,5 +51,14 @@ angular.module('app').config(function ($stateProvider) {
                 }
             }
         }
+    }).state('app.checklist', {
+        views: {
+            app: {
+                controller: 'app_checklist',
+                templateProvider: function (app) {
+                    return app.templateProvider('app.checklist');
+                }
+            }
+        }
     });
 });
