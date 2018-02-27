d7e57fe47e3ce425c9c59901fe67aa76744a3488
Kevin Schlosser <kevin.schlosser@flex.com>
2018-02-27 03:36:51 -0800

    [Studio] Delete screen "app.checklist"

--- client/states.js
+++ client/states.js
@@ -42,14 +42,5 @@ angular.module('app').config(function ($stateProvider) {
                 }
             }
         }
-    }).state('app.checklist', {
-        views: {
-            app: {
-                controller: 'app_checklist',
-                templateProvider: function (app) {
-                    return app.templateProvider('app.checklist');
-                }
-            }
-        }
     });
 });
