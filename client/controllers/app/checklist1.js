4a33e9dd31edd8c9cc69addc0e926c9d06773255
Kevin Schlosser <kevin.schlosser@flex.com>
2018-02-27 03:39:44 -0800

    [Extension] Create screen "checklist1"

---
+++ client/controllers/app/checklist1.js
@@ -0,0 +1,5 @@
+angular.module('app').controller('app_checklist1', app_checklist1);
+function app_checklist1($scope, app) {
+    'use strict';
+    app.init($scope);
+}
--- client/states.js
+++ client/states.js
@@ -42,5 +42,14 @@ angular.module('app').config(function ($stateProvider) {
                 }
             }
         }
+    }).state('app.checklist1', {
+        views: {
+            app: {
+                controller: 'app_checklist1',
+                templateProvider: function (app) {
+                    return app.templateProvider('app.checklist1');
+                }
+            }
+        }
     });
 });
