aeaa8b023903b4f26d642295e6c2f6b684adc814
Kevin Schlosser <kevin.schlosser@flex.com>
2018-02-27 03:10:59 -0800

    [Studio] Delete screen model "mytimetable"

--- client/mocks/mytimetable.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-window.mocks = window.mocks || {};
-window.mocks.mytimetable = window.mocks.mytimetable || {};
-window.mocks.mytimetable.mytimetable_ChooseWeek = function ($scope, params, app) {
-};
