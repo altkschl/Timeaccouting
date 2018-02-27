5e55ac2a01ad1b856854f7a0ae4ec9f26ecad21c
Kevin Schlosser <kevin.schlosser@flex.com>
2018-02-27 03:11:18 -0800

    [Studio] Delete screen model "enterhourspopup"

--- client/mocks/enterhourspopup.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-window.mocks = window.mocks || {};
-window.mocks.enterhourspopup = window.mocks.enterhourspopup || {};
-window.mocks.enterhourspopup.clickConfirm = function ($scope, params, app) {
-    alert('Hours allocated !');
-};
