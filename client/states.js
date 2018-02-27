e8bc9283a0d97d880bb595fecbc90631b1d1a3cd
Kevin Schlosser <kevin.schlosser@flex.com>
2018-02-27 03:11:10 -0800

    [Studio] Delete screen "app.enterhourspopup"

--- client/states.js
+++ client/states.js
@@ -42,15 +42,6 @@ angular.module('app').config(function ($stateProvider) {
                 }
             }
         }
-    }).state('app.enterhourspopup', {
-        views: {
-            app: {
-                controller: 'app_enterhourspopup',
-                templateProvider: function (app) {
-                    return app.templateProvider('app.enterhourspopup');
-                }
-            }
-        }
     }).state('app.fdsyf', {
         views: {
             app: {
